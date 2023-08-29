64d569f5a672d55aeabd202389edd2a93b69960f
ERIKABUDIARTI <138299266+ERIKABUDIARTI@users.noreply.github.com>
2023-08-29 12:46:38 +0700

    Update heart-disease_v.2.py

--- heart-disease_v.2.py
+++ heart-disease_v.2.py
@@ -111,7 +111,7 @@ if loaded_model is not None:
             results.append('Yes Heart Disease')
         st.write("Prediction Results:")
         for idx, result in enumerate(results):
-            st.write(f"Sample {idx + 1}: {result}")
+            st.write(f"{idx + 1}: {result}")
 
         #result = ['No Heart Disease' if prediction == 0 else 'Yes Heart Disease']
         #output = str(result[0])
